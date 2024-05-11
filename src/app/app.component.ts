d608f1d6166e1261f2ea2c8cf551c932a65ab8a6
shoyosd <sebastianhoyosduran@gmail.com>
2024-05-10 21:43:57 -0500

    upload project to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'CURSO DE ANGULAR MIC-TIC';
+  valor: number = 0;
+  darMensaje(){
+    alert("Saludos desde fosca");
+  }
+  incrementar(){
+    this.valor ++;
+  }
+  decrementar(){
+    this.valor--;
+  }
 }
---
+++ src/app/pages/page1/page1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  standalone: true,
+  imports: [],
+  templateUrl: './page1.component.html',
+  styleUrl: './page1.component.css',
+})
+export class Page1Component {}
